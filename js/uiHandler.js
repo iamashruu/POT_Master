cd28c0594e6a6250e6ee6086209960bdc8d4ed96
Ashraf <54413503+iamashruu@users.noreply.github.com>
2024-11-15 15:06:54 +0600

    added table header and copied warning

--- js/uiHandler.js
+++ js/uiHandler.js
@@ -1,11 +1,35 @@
 // uiHandler.js
-export function displayOutput(output) {
+export function displayOutput(output,rrn,date) {
     const outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "";
+
     if (output.length === 0) {
         const notFoundText = document.createTextNode("Sorry, not found! Please check MTIs and RRN.");
         outputDiv.appendChild(notFoundText);
     } else {
+        outputDiv.innerHTML = `<table class="styled-table">
+                                <tr>
+                                    <td class="label-cell">Date</td>
+                                    <td>${date}</td>
+                                </tr>
+                                <tr>
+                                    <td class="label-cell">Test Case</td>
+                                    <td> </td>
+                                </tr>
+                                <tr>
+                                    <td class="label-cell">RRN</td>
+                                    <td>${rrn}</td>
+                                </tr>
+                                <tr>
+                                    <td class="label-cell">Result</td>
+                                    <td>Approved</td>
+                                </tr>
+                                <tr>
+                                    <td class="label-cell">Tested By</td>
+                                    <td>Fintech Point</td>
+                                </tr>
+                            </table>`;
+
         output.forEach((line) => {
             // Create a temporary element to easily strip any HTML like &nbsp;
             const tempElement = document.createElement("div");
@@ -21,7 +45,7 @@ export function displayOutput(output) {
                 const lineElement = document.createElement("p");
                 lineElement.innerHTML = line;
                 outputDiv.appendChild(lineElement);
-                console.log('Adding valid line:', line);
+                // console.log('Adding valid line:', line);
             }
         });  
     }
@@ -51,7 +75,20 @@ function copyToClipboard() {
     selection.addRange(range);
     try {
         document.execCommand("copy");
-        alert("Results copied to clipboard!");
+        // alert("Results copied to clipboard!");
+        // Change button text to "Copied!" for 3 seconds
+        const copyButton = document.getElementById("copyButton");
+        const originalText = copyButton.textContent;
+        copyButton.textContent = "Copied!";
+        copyButton.style.backgroundColor = '#126186';
+        copyButton.disabled = true; // Disable the button temporarily
+
+        // Revert the button text after 3 seconds
+        setTimeout(() => {
+            copyButton.textContent = originalText;
+            copyButton.style.backgroundColor = '#4caf50';
+            copyButton.disabled = false; // Enable the button again
+        }, 1500);
     } catch (err) {
         alert("Unable to copy to clipboard.");
     }
